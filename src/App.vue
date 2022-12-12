25d6b85ac963e007cd346a9dc73880acc91d56a8
Kuroro <takehiko.suzume@yandex.ru>
2022-12-12 15:05:28 +0300

    Добавил ссылки (+адаптив)

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,46 @@
   <!-- App.vue -->
 
   <v-app>
-    <v-navigation-drawer app temporary>
+    <v-navigation-drawer
+      app
+      temporary
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-item
+          :to="link.url"
+          v-for="link of links"
+          :key="link.title"
+        >
+          <v-list-item-icon>
+            <v-icon>
+              {{ link.icon }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app dark color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        class="hidden-md-and-up"
+      ></v-app-bar-nav-icon>
       <v-toolbar-title>Adv application</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn text>Link One</v-btn>
+        <v-btn
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url"
+          text
+        >
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
       </v-toolbar-items>
     </v-app-bar>
 
@@ -23,7 +54,18 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {title: 'login', icon: 'mdi-lock', url: '/login'},
+        {title: 'Registration', icon: 'mdi-face-man', url: '/registration'},
+        {title: 'Orders', icon: 'mdi-bookmark-multiple-outline', url: '/orders'},
+        {title: 'New ad', icon: 'mdi-file-document-plus-outline', url: '/new'},
+        {title: 'My ads', icon: 'mdi-format-list-bulleted', url: '/list'}
+      ]
+    }
+  }
 }
 </script>
 
